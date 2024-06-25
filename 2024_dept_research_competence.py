52b26bd0bfff473970d667f50215e52ead13b7e6
Juneho0629 <102876293+Juneho0629@users.noreply.github.com>
2024-06-25 11:43:24 +0900

    Update 2024_dept_research_competence.py

--- 2024_dept_research_competence.py
+++ 2024_dept_research_competence.py
@@ -20,7 +20,8 @@ connection = mysql.connector.connect(
     host = st.secrets['mysql']['db_host'],
     user = st.secrets['mysql']['db_user'],
     password = st.secrets['mysql']['db_password'],
-    database = st.secrets['mysql']['db_database']
+    database = st.secrets['mysql']['db_database'],
+    port = st.secrets['mysql']['db_port']
 )
 
 st.markdown(streamlit_style, unsafe_allow_html=True)
